134634327411382fa8757973ef694d5990f7baa6
Roshan Padit <roshanpadit2004@gmail.com>
2025-08-17 21:19:21 +0530

    modified:   about.html 	modified:   contact.html 	modified:   dashboard.html 	modified:   events.html 	modified:   faq.html 	modified:   gallery.html 	modified:   index.html 	modified:   login-register.html 	modified:   privacy-policy.html 	modified:   register.html 	modified:   script.js 	modified:   sponsors.html 	modified:   style.css 	modified:   team.html 	modified:   terms.html

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const observer = new IntersectionObserver((entries) => {
             entry.target.classList.add('show');
         }
     });
-});
+}, { threshold: 0.1 });
 
 const hiddenElements = document.querySelectorAll('.category-link, .team-member, .sponsor-logo');
 hiddenElements.forEach((el) => observer.observe(el));
